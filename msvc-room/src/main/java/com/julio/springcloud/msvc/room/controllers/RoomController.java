7d3a0e0c16ad28a588c3daffe7cf03ef98d00da3
JulioKrack <julio.ruiz.dev@gmail.com>
2024-10-19 11:12:22 -0500

    se modifico msvc rooms para subir imagen

--- msvc-room/src/main/java/com/julio/springcloud/msvc/room/controllers/RoomController.java
+++ msvc-room/src/main/java/com/julio/springcloud/msvc/room/controllers/RoomController.java
@@ -1,12 +1,20 @@
 package com.julio.springcloud.msvc.room.controllers;
 
+import org.apache.hc.client5.http.auth.KerberosConfig.Option;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.julio.springcloud.msvc.room.entities.Room;
 import com.julio.springcloud.msvc.room.services.RoomService;
 
+import jakarta.validation.Valid;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,9 +39,46 @@ public class RoomController {
     }
 
     // Crear o actualizar una habitación
-    @PostMapping
-    public Room saveRoom(@RequestBody Room room) {
-        return roomService.save(room);
+    @PostMapping("/crear-con-foto")
+    public ResponseEntity<?> saveWithPhotoRoom(@Valid Room room, BindingResult result,
+            @RequestParam MultipartFile file) throws IOException {
+        if (result.hasErrors()) {
+            return ResponseEntity.badRequest().body(result);
+        }
+
+        if (!file.isEmpty()) {
+            room.setFoto(file.getBytes());
+        }
+        Room roomDB = roomService.save(room);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(roomDB);
+    }
+
+    @PutMapping("/editar-con-foto/{id}")
+    public ResponseEntity<?> editWithPhotoRoom(@Valid Room room, BindingResult result,
+            @RequestParam MultipartFile file) throws IOException {
+        if (result.hasErrors()) {
+            return ResponseEntity.badRequest().body(result);
+        }
+
+        Optional<Room> o = roomService.findById(room.getId());
+
+        if (o.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Room roomDB = o.get();
+        roomDB.setRoomType(room.getRoomType());
+        roomDB.setCapacity(room.getCapacity());
+        roomDB.setDescription(room.getDescription());
+        roomDB.setPrice(room.getPrice());
+        roomDB.setStatus(room.isStatus());
+
+        if (!file.isEmpty()) {
+            roomDB.setFoto(file.getBytes());
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(roomService.save(roomDB));
     }
 
     // Eliminar una habitación por ID
@@ -48,4 +93,5 @@ public class RoomController {
     public List<Room> getRoomsByType(@RequestParam String roomType) {
         return roomService.findByRoomType(roomType);
     }
+
 }
--- msvc-room/src/main/java/com/julio/springcloud/msvc/room/entities/Room.java
+++ msvc-room/src/main/java/com/julio/springcloud/msvc/room/entities/Room.java
@@ -1,7 +1,10 @@
 package com.julio.springcloud.msvc.room.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "rooms")
@@ -18,24 +21,70 @@ public class Room {
     @Min(value = 1, message = "El valor minimo es 1")
     private Integer capacity;
 
+    @NotBlank(message = "La descripcion es requerida")
+    private String description;
+
+    @NotNull(message = "El estado es requerido")
+    private boolean status;
+
     @NotNull(message = "El precio es requerido")
     @DecimalMin(value = "0.0", inclusive = false, message = "El precio minimo es 1")
     private Double price;
 
-    public Room() {
+    @Lob
+    @JsonIgnore
+    private byte[] foto;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createAt = new Date();
     }
 
-    public Room(Long id, @NotBlank(message = "Habitacion es requerida") String roomType,
-            @NotNull(message = " La capacidad es requerida") @Min(value = 1, message = "El valor minimo es 1") Integer capacity,
-            @NotNull(message = "El precio es requerido") @DecimalMin(value = "0.0", inclusive = false, message = "El precio minimo es 1") Double price) {
-        this.id = id;
-        this.roomType = roomType;
-        this.capacity = capacity;
-        this.price = price;
+    public Integer getFotoHashCode() {
+        return (this.foto != null) ? this.foto.hashCode() : null;
+    }
+
+    public Room() {
     }
 
     public Long getId() {
         return id;
+    }
+
+    public Room(Long id, @NotBlank(message = "Habitacion es requerida") String roomType,
+            @NotNull(message = " La capacidad es requerida") @Min(value = 1, message = "El valor minimo es 1") Integer capacity,
+            @NotBlank(message = "La descripcion es requerida") String description,
+            @NotNull(message = "El estado es requerido") boolean status,
+            @NotNull(message = "El precio es requerido") @DecimalMin(value = "0.0", inclusive = false, message = "El precio minimo es 1") Double price,
+            byte[] foto, Date createAt) {
+        this.id = id;
+        this.roomType = roomType;
+        this.capacity = capacity;
+        this.description = description;
+        this.status = status;
+        this.price = price;
+        this.foto = foto;
+        this.createAt = createAt;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 
     public void setId(Long id) {
@@ -64,6 +113,22 @@ public class Room {
 
     public void setPrice(Double price) {
         this.price = price;
+    }
+
+    public byte[] getFoto() {
+        return foto;
+    }
+
+    public void setFoto(byte[] foto) {
+        this.foto = foto;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
     }
 
 }
